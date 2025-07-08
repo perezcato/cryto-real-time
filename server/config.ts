22bc077978122292ce77c32f53457a5ab4e5b04e
perezcato <perezcatoc@gmail.com>
2025-07-08 23:37:34 +0200

    Update server port configuration to ensure it is a number

--- server/config.ts
+++ server/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-export const PORT = process.env.PORT || 8080;
+export const PORT = Number(process.env.PORT) || 8080;
 export const BINANCE_API_URL = "wss://stream.binance.com:9443/ws";
 export const ASSETS = ["btc", "eth", "sol"];
 export const RECONNECT_DELAY = 5000;
